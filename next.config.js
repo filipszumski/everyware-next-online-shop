cffd01be0eb9fb0abb11d6647eb3ce139d52721a
Filip Szumski <filipszumski@gmail.com>
2026-03-20 12:22:12 +0100

    chore: create vercel config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["next-mdx-remote"],
   images: {
     domains: ["naszsklep-api.vercel.app", "media.graphassets.com"],
     formats: ["image/avif", "image/webp"],
